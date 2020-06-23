0b3a6751951113c417f89ddc7cd506e0b4e398ed
chitesh <chiteshbansal111@gmail.com>
2020-06-23 13:38:57 +0530

    Updates

---
+++ precache-manifest.5c82233e62fccd815d5e855398b28d82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4ea437b57ad04eeb8242a8a39918cd",
+    "url": "/ta_app/index.html"
+  },
+  {
+    "revision": "0b6b9d6da17c46c15a5d",
+    "url": "/ta_app/static/css/main.e50234df.chunk.css"
+  },
+  {
+    "revision": "627f3c17f663807ab214",
+    "url": "/ta_app/static/js/2.acdaa4c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ta_app/static/js/2.acdaa4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b9d6da17c46c15a5d",
+    "url": "/ta_app/static/js/main.c84d023c.chunk.js"
+  },
+  {
+    "revision": "59db07d332f285b8ee82",
+    "url": "/ta_app/static/js/runtime-main.ca2dc546.js"
+  },
+  {
+    "revision": "070b982f406bc5b5569a435a6f8674d4",
+    "url": "/ta_app/static/media/deeplearning.070b982f.jpg"
+  }
+]);
